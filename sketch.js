6fb1b320cb9f1e32b1944e895432b51f9f6a13c7
dharmiksanthosh <dharmiksanthosh@gamil.com>
2020-11-01 11:59:59 +0530

    C:/Program Files/Git/

--- sketch.js
+++ sketch.js
@@ -1,6 +1,15 @@
+var dev,x = 500,y = 300,up,down,right,left;
+
 function setup(){
     createCanvas(1000,600);
 
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        
+        dev = "mobile"
+    }else{
+
+        dev = "pc"
+    }
 }
 
 function draw(){
@@ -8,12 +17,40 @@ function draw(){
     fill(255)
     textSize(20);
    
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    if (dev==="pc") {
         
-        text("This is a Mobile",400,300);
+        if (keyIsDown(UP_ARROW)) {
+            
+            y -= 5;
+        }
+        if (keyIsDown(DOWN_ARROW)) {
+            
+            y += 5;
+        }
+        if (keyIsDown(LEFT_ARROW)) {
+            
+            x -= 5;
+        }
+        if (keyIsDown(RIGHT_ARROW)) {
+            
+            x += 5;
+        }
+
     }else{
 
-        text("This is a PC",400,300);
+        up = createButton("Up");
+        down = createButton("Down");
+        right = createButton("Right");
+        left = createButton("Left");
+
+        up.position(850,400);
+        down.position(850,500);
+        left.position(750,450);
+        right.position(950,450);
     }
+
+    fill("red");
+    rectMode(CENTER);
+    rect(x,y,50,50);
 }
 
