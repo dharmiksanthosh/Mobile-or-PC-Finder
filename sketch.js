e31fad8c6f538717af9f4e562cff7347649f863c
dharmiksanthosh <dharmiksanthosh@gamil.com>
2020-11-01 12:46:38 +0530

    Bug Fixes

--- sketch.js
+++ sketch.js
@@ -3,13 +3,7 @@ var dev,x = 500,y = 300,up,down,right,left;
 function setup(){
     createCanvas(1000,600);
 
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        
-        dev = "mobile"
-    }else{
-
-        dev = "pc"
-    }
+    
 }
 
 function draw(){
@@ -43,10 +37,38 @@ function draw(){
         right = createButton("Right");
         left = createButton("Left");
 
-        up.position(850,400);
-        down.position(850,500);
-        left.position(750,450);
-        right.position(950,450);
+        up.position(845,420);
+        down.position(845,520);
+        left.position(795,470);
+        right.position(895,470);
+
+        fill(240,240,240)
+        noStroke();
+        rectMode(CENTER);
+        rect(870,490,50,60)
+
+        up.size(50,50);
+        down.size(50,50);
+        right.size(50,50);
+        left.size(50,50);
+
+        up.mousePressed(() => {
+            
+            y -= 5;
+        })
+        down.mousePressed(() => {
+            
+            y += 5;
+        })
+        right.mousePressed(() => {
+            
+            x += 5;
+        })
+        left.mousePressed(() => {
+            
+            x -= 5;
+        })
+
     }
 
     fill("red");
